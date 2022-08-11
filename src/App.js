17b22e23ed22226bdc8ca54ef7d44b6ec07682fc
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-08-11 03:01:26 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import videoSample from './videos/sample.mp4'
 import './App.css';
+import { useRef } from 'react'
 
 function App() {
+  const video = useRef()
+
+  let playStatus = false
+
+  const handlePlay = () => {
+    console.log(playStatus)
+    if (playStatus) {
+      video.current.play()
+      playStatus = false
+    } else {
+      video.current.pause()
+      playStatus = true
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video src={videoSample} id="video" ref={video}></video>
+      <button onClick={handlePlay}>{playStatus ? 'Pause' : 'Play'}</button>
     </div>
   );
 }
