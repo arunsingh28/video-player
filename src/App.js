6d62d65cbd81d9d5dbba661bb87c2dcb952a5cfa
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-08-11 05:49:31 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import videoSample from './videos/sample.mp4'
 import './App.css';
-import { useRef } from 'react'
+import './player.css'
+import { Player } from 'video-react';
 
 function App() {
-  const video = useRef()
-
-  let playStatus = false
-
-  const handlePlay = () => {
-    console.log(playStatus)
-    if (playStatus) {
-      video.current.play()
-      playStatus = false
-    } else {
-      video.current.pause()
-      playStatus = true
-    }
-  }
 
   return (
     <div className="App">
-      <video src={videoSample} id="video" ref={video}></video>
-      <button onClick={handlePlay}>{playStatus ? 'Pause' : 'Play'}</button>
+      <Player>
+        <source src={videoSample} />
+      </Player>
     </div>
-  );
+  )
 }
 
 export default App;
